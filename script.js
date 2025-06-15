2b0f3d87eb69cc8fe9e7a51e6e3570fb8e6653e0
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-06-15 14:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
     input.addEventListener('input', (e) => {
-        // Check if the input value length is 1 and move focus to the next input
+        console.log("Input value: ", e.target.value);
         if (e.target.value.length === 1 && index < inputs.length - 1) {
             inputs[index + 1].focus();
+            console.log("Focused on: ", inputs[index + 1].id);
         }
     });
 
     input.addEventListener('keydown', (e) => {
-        // If backspace is pressed and the current input is empty, move focus to the previous input
         if (e.key === 'Backspace' && index > 0 && input.value === '') {
             inputs[index - 1].focus();
+            console.log("Focused back on: ", inputs[index - 1].id);
         }
+		describe('OTP Input Test', () => {
+    it('Checking Forward Typing', () => {
+        cy.visit('path_to_your_index.html'); // Apne HTML file ka path yahan daalo
+        cy.get('#code-1').type('1').should('have.value', '1');
+        cy.get('#code-2').should('have.focus'); // Check if focus is on code-2
+    });
+});
     });
 });
