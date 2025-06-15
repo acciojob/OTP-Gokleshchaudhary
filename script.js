7075c326d0b0d383a549f01c0c3024be8a79cb1c
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-06-15 14:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+// script.js
 const inputs = document.querySelectorAll('.code');
 
 inputs.forEach((input, index) => {
     input.addEventListener('input', (e) => {
-        console.log("Input value: ", e.target.value);
+        // Check if the input value length is 1 and move focus to the next input
         if (e.target.value.length === 1 && index < inputs.length - 1) {
             inputs[index + 1].focus();
-            console.log("Focused on: ", inputs[index + 1].id);
         }
     });
 
     input.addEventListener('keydown', (e) => {
+        // If backspace is pressed and the current input is empty, move focus to the previous input
         if (e.key === 'Backspace' && index > 0 && input.value === '') {
             inputs[index - 1].focus();
         }
